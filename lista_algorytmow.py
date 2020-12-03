214093d18ca0e88c533870260d04cf1f66704b64
Pawel <poczta130@gmail.com>
2020-12-03 17:44:35 +0100

    Update lista_algorytmow.py

--- lista_algorytmow.py
+++ lista_algorytmow.py
@@ -5,3 +5,17 @@ def decimalToBinary(num):
         num_+=str(num % 2)
     
     return num_
+
+
+
+def is_prime(n):
+	if n<2:
+		return False
+	else:
+		N = n//2 +1 
+		for x in range(2, N):
+			if n%x==0:
+				return False
+
+
+		return True
